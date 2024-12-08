897a628bec57c0d712d081a6d5ede6610be8659b
Balakrishnan T <126885624+balakrishnanthirumal@users.noreply.github.com>
2024-12-08 11:00:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,11 +15,13 @@ const port = process.env.PORT || 3000;
 connectDB();
 
 const app = express();
-// app.use(cors({
-//   origin: 'https://balexstore-frontend-obzb.onrender.com',
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// }));
+const corsOptions = {
+  origin: 'https://balexstore-frontend-obzb.onrender.com', // Replace with your frontend domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  credentials: true, 
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
